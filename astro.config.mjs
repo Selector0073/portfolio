3ccbe576472dfcfeb55c2505dcda39ddcebf30f5
Selector0073 <97699633+Selector0073@users.noreply.github.com>
2026-06-19 15:01:47 +0300

    auto xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://selector0073-portfolio.netlify.app',
+  integrations: [
+    sitemap({
+      filter: (page) => page !== 'https://selector0073-portfolio.netlify.app/privacy/',
+    }),
+  ],
+});
